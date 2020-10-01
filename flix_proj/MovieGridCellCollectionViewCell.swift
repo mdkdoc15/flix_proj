32b12e00c517a12b0b27b16d05429866a7b620e6
Matthew Kight <kightmat@msu.edu>
2020-10-01 11:28:35 -0400

    Final Design with no extra Features

---
+++ flix_proj/MovieGridCellCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  MovieGridCellCollectionViewCell.swift
+//  flix_proj
+//
+//  Created by Matthew Kight on 9/30/20.
+//
+
+import UIKit
+
+class MovieGridCellCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+    
+}
